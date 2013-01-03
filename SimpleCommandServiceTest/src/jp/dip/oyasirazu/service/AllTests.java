66c053538638918c36453604eea25b3413e12f25
mikoto2000 <mikoto2000@gmail.com>
2013-01-04 00:34:56 +0900

    Added "build.xml" targets.
    Added target:
    - test-compile
    - test-run

---
+++ SimpleCommandServiceTest/src/jp/dip/oyasirazu/service/AllTests.java
@@ -0,0 +1,11 @@
+package jp.dip.oyasirazu.service;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SimpleCommandServiceTest.class})
+public class AllTests {
+}
